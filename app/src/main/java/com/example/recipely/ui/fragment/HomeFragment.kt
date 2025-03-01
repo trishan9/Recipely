4087a0bdf13b62b953d15bc7d1375512ea8516e1
Trishan Wagle <mailtotrishan@gmail.com>
2025-03-01 16:43:51 +0545

    feat: recipe model

--- app/src/main/java/com/example/recipely/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/recipely/ui/fragment/HomeFragment.kt
@@ -6,12 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.recipely.R
+import com.example.recipely.databinding.FragmentHomeBinding
+import com.example.recipely.databinding.FragmentProfileBinding
+import com.example.recipely.viewmodel.UserViewModel
+
 class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var userViewModel: UserViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/recipely/ui/fragment/RecipesFragment.kt
+++ app/src/main/java/com/example/recipely/ui/fragment/RecipesFragment.kt
@@ -6,13 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.recipely.R
+import com.example.recipely.databinding.FragmentBookmarksBinding
+import com.example.recipely.databinding.FragmentProfileBinding
+import com.example.recipely.databinding.FragmentRecipesBinding
+import com.example.recipely.viewmodel.UserViewModel
 
 class RecipesFragment : Fragment() {
+    private var _binding: FragmentRecipesBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var userViewModel: UserViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        _binding = FragmentRecipesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
